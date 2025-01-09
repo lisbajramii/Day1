086357c8f5cdde572c746a9869ff288ab2e66c45
lisbajramii <onlb.lis0@gmail.com>
2025-01-09 18:57:51 +0100

    sdg
    dsg

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('goodbye')
+
+
+var _test=5;
+
+var Test = 4;
+var $test = 4;
+//var 0test=4;
+var t0est = 4;
+var value1=4;
+var TEST =6;
+var test = 4;
+
+
+console.log(TEST)
+console.log(test)
+
+var a = 50 + 50;
+
+console.log(a)
