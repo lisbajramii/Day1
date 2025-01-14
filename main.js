6cc7205216bae2115aaf42612258112d59c76666
lisbajramii <onlb.lis0@gmail.com>
2025-01-14 19:13:52 +0100

    dfhadhf
    dfhdfash

--- main.js
+++ main.js
@@ -1,20 +1,90 @@
-console.log('goodbye')
+// console.log('goodbye')
 
 
-var _test=5;
+// var _test=5;
 
-var Test = 4;
-var $test = 4;
-//var 0test=4;
-var t0est = 4;
-var value1=4;
-var TEST =6;
-var test = 4;
+// var Test = 4;
+// var $test = 4;
+// //var 0test=4;
+// var t0est = 4;
+// var value1=4;
+// var TEST =6;
+// var test = 4;
 
 
-console.log(TEST)
-console.log(test)
+// console.log(TEST)
+// console.log(test)
 
-var a = 50 + 50;
+// var a = 50 + 50;
+
+// console.log(a)
+
+
+var r =10;
+r-= 5;
+
+console.log(r)
+
+
+var s =30;
+s+= 15;
+
+console.log(s)
+
+
+var a =25;
+a*= 10;
 
 console.log(a)
+
+
+
+
+var t =10;
+t/= 5;
+
+console.log(t)
+
+
+var y=20;
+y%= 10;
+
+console.log(y)
+
+
+var o =5;
+var u =5;
+
+console.log(o == u)
+
+
+
+var p =6;
+var l =5;
+
+console.log(p == l)
+
+
+var m =5;
+var n =5;
+
+console,log(m != n)
+
+
+var num1 =100;
+var num2 =10;
+
+console.log(num1 > num2)
+
+
+var num3 =100;
+var num4 =10;
+
+console.log(num1 < num2)
+
+
+
+var num5 =10;
+var num6 ='10';
+
+console.log(num5===num6)
